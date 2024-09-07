891a7193a4ec4ed0ec5e3b6db6c8706d591bbb4e
APaud3l <aamod.paudel@coderacademy.edu.au>
2024-09-07 10:29:30 +1000

    Replaced the authorise_as_admin fn with decorator implementation

--- controllers/card_controller.py
+++ controllers/card_controller.py
@@ -7,8 +7,9 @@ from init import db
 from models.card import Card, card_schema, cards_schema
 
 from controllers.comment_controller import comments_bp
+from utils import auth_as_admin_decorator
 
-from utils import authorise_as_admin
+# from utils import authorise_as_admin
 
 cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
 cards_bp.register_blueprint(comments_bp)
@@ -61,9 +62,10 @@ def create_card():
 # /cards/<id> - DELETE - delete a card
 @cards_bp.route("/<int:card_id>", methods=["DELETE"])
 @jwt_required()
+@auth_as_admin_decorator
 def delete_card(card_id):
     # check whether the user is admin or not
-    is_admin = authorise_as_admin()
+    # is_admin = authorise_as_admin()
     # if not admin
     if not is_admin:
         # return error message
@@ -86,6 +88,7 @@ def delete_card(card_id):
 # /cards/<id> - PUT, PATCH - edit a card entry
 @cards_bp.route("/<int:card_id>", methods=["PUT", "PATCH"])
 @jwt_required()
+@auth_as_admin_decorator
 def update_card(card_id):
     # get the info from the body of the request
     body_data = card_schema.load(request.get_json(), partial=True)
@@ -93,7 +96,7 @@ def update_card(card_id):
     stmt = db.select(Card).filter_by(id=card_id)
     card = db.session.scalar(stmt)
     # check whether the user is admin or not
-    is_admin = authorise_as_admin()
+    # is_admin = authorise_as_admin()
     # if the card exists
     if card:
         # if the user is not the owner of the card
--- utils.py
+++ utils.py
@@ -5,14 +5,14 @@ import functools
 from init import db
 from models.user import User
 
-def authorise_as_admin():
-    # get the user's id from get_jwt_identity
-    user_id = get_jwt_identity()
-    # fetch the user from the db
-    stmt = db.select(User).filter_by(id=user_id)
-    user = db.session.scalar(stmt)
-    # check whether the user is an admin or not
-    return user.is_admin
+# def authorise_as_admin():
+#     # get the user's id from get_jwt_identity
+#     user_id = get_jwt_identity()
+#     # fetch the user from the db
+#     stmt = db.select(User).filter_by(id=user_id)
+#     user = db.session.scalar(stmt)
+#     # check whether the user is an admin or not
+#     return user.is_admin
 
 # Creating a decorator for authorise_as_admin
 
